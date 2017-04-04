305c82a8db2e5d74d1c8c27ebddf28eba90dfea0
Janhunen <brianpence12@gmail.com>
2017-04-04 16:01:32 +0100

    Update urls.py

--- growthstreet/urls.py
+++ growthstreet/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from registration.backends.default.views import RegistrationView
+from registration.forms import RegistrationFormUniqueEmail
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name='registration_register'),
+    url(r'^admin/', admin.site.urls),
+	url(r'^accounts/', include('registration.backends.default.urls')),
+	url(r'^finance/', include('demo.urls'))
 ]
